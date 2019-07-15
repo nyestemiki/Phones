7bd31c629af56e8041697d8da15000e8d9d4e37e
Miki Nyeste <nyeste.miki@yahoo.com>
2019-07-15 23:25:20 +0300

    Brand Selector improvements

--- script.js
+++ script.js
@@ -1,32 +1,52 @@
 const brands = document.querySelectorAll('.brand');
 const logo = document.querySelector('.logo img');
-const images = document.querySelectorAll('.phones img');
+const images = [...document.querySelectorAll('.phones img')];
 
 function selectPhone() {
+    // Making the selected brand full-screen
     this.style.width =  document.body.getBoundingClientRect() + "px";
+    // Hiding the not-selected brands
     brands.forEach(brand => {
-        if (brand !== this)
+        if (brand !== this) {
            brand.style.width = 0;
+        }
     });
+    // Animating out the images
+    setTimeout(() => {
+        images.forEach(image => {
+            if(image.dataset.brand !== this.dataset.brand)
+                image.style.opacity = 0;
+        })
+    }, 1000);
+    // Hiding the not-selected brands
     setTimeout(() => {
         brands.forEach(brand => {
             if (brand !== this) {
                 brand.style.display = "none";
             }
         })
-    }, 1500); // Change to transition of fullscreening
+    }, 2000); // Change to transition of fullscreening
 }
 
 function startPage() {
+    // Display the hidden brands
     brands.forEach(brand => {
         if (brand !== this)
             brand.style.display = "grid";
     });
+    // Animating in the hidden brands
     setTimeout(() => {
         brands.forEach(brand => {
             brand.style.width = "100%";
-        })
+        });
     }, 200); 
+    // Animating in the hidden images
+    setTimeout(()=> {
+        images.forEach(image => {
+            if(image.style.opacity == 0)
+                image.style.opacity = 1.0;
+        });
+    }, 500);
 }
 
 brands.forEach(brand => brand.addEventListener('click', selectPhone));
