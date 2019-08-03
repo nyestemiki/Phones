b9f2b2048b9bb6072153d6d8f294ee9922d42b7c
nyestemiki <nyeste.miki@yahoo.com>
2019-08-03 04:30:33 +0300

    Deactivating events when transitioning

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 const brands = document.querySelector('.brands');
 const logo = document.querySelector('.logo img');
+let isAnimating = false;
 
 // Loading the brands + eventlisteners to select them
 $.getJSON("data.json", data => {
     let index = 1;
+
     // Loading brands
     Object.keys(data.brands).forEach(brand => {
         brands.innerHTML += `
-            <div class="brand brand${index}" data-brand="${index++}">
+            <div class="brand brand_hover brand${index}" data-brand="${index++}">
                 <div class="img">
                     <img src="${data.brands[brand].cover}">
                 </div>
@@ -16,14 +18,48 @@ $.getJSON("data.json", data => {
                 </div>
             </div>`;
     });
+
     // Appending eventlisteners
     document.querySelectorAll('.brand').forEach(brand => {
-        brand.addEventListener('click', () => hideBrandsExceptFor(brand));
+        brand.addEventListener('click', () => selectBrand(brand));
     });
 });
 
+function selectBrand(brand) {
+    // Waiting for animation to terminate
+    if (isAnimating) {
+        return;
+    }
+
+    removeBrandHovers();
+    hideBrandsExceptFor(brand);
+    changeTitleTo(brand);
+}
+
+// Changes window's title to brandname
+function changeTitleTo(brand) {
+    document.title = brand.querySelector('.title').textContent;
+}
+
+// Removes hover effects from the brands
+function removeBrandHovers() {
+    [...document.querySelectorAll('.brand')].map(instance => {
+        instance.classList.remove('brand_hover');
+    });
+}
+
+// Reactivates hover effects on the brands
+function addBrandHovers() {
+    [...document.querySelectorAll('.brand')].map(instance => {
+        instance.classList.add('brand_hover');
+    });
+}
+
 // Hides the brands except for the given 'brand'
 function hideBrandsExceptFor(brand) {
+    //Animation started
+    isAnimating = true;
+
     // Fading out content of brand container
     [...document.querySelectorAll('.brand')].map(instance => {
         if (instance !== brand) {
@@ -48,10 +84,20 @@ function hideBrandsExceptFor(brand) {
             }
         });
     }, 750);
+
+    // Animation terminated
+    setTimeout(() => {
+        isAnimating = false;
+    }, 2500);
 }
 
 // Displaying the main menu with all the brands
 function mainMenu() {
+    window.title = "Phones";
+
+    // Animation started
+    isAnimating = true;
+
     // Activating hidden brands
     [...document.querySelectorAll('.brand')].map(instance => {
         instance.classList.remove('brand_hide');
@@ -70,6 +116,16 @@ function mainMenu() {
             instance.classList.remove('content_fade');
         });
     }, 1000);
+
+    // Reactivating hover effects on brands 
+    setTimeout(() => {
+        addBrandHovers();
+    }, 1200);
+
+    // Animation terminated
+    setTimeout(() => {
+        isAnimating = false;
+    }, 2500);
 }
 
 // Click on logo => back to main menu
