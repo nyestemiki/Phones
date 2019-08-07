17656481bed4cb36039db3366e210c44a17622f6
nyestemiki <nyeste.miki@yahoo.com>
2019-08-07 16:52:50 +0100

    Sliding dropdown

--- script.js
+++ script.js
@@ -425,10 +425,23 @@ const nav = document.querySelector('.nav');
 
 function handleEnter() {
     this.classList.add('trigger-enter');
-    setTimeout(() => this.classList.add('trigger-enter-active'), 150);
+    setTimeout(() => this.classList.contains('trigger-enter') && 
+            this.classList.add('trigger-enter-active'), 300);
     background.classList.add('dropdown_open');
 
-    const dropdown = this.querySelector('.tile_container');             
+    const dropdown = this.querySelector('.tile_container');           
+    const dropdownCoords = dropdown.getBoundingClientRect();
+    const navCoords = nav.getBoundingClientRect();
+    const coords = {
+        height: dropdownCoords.height,
+        width: dropdownCoords.width,
+        top: dropdownCoords.top - navCoords.top,
+        left: dropdownCoords.left - navCoords.left
+    };
+    background.style.setProperty('width', `${coords.width}px`);
+    background.style.setProperty('height', `${coords.height}px`);  
+    background.style.setProperty('transform', `translate(${coords.left}px, ${coords.top}px`);
+    background.style.setProperty('height', `${coords.height}px`);  
 }
 
 function handleLeave() {
